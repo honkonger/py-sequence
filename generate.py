be61070e97917496dbbacebbbb95b5b06004d8c2
bjørnar <bjoernar.kk@gmail.com>
2024-04-28 13:55:02 +0200

    remve print lists

--- generate.py
+++ generate.py
@@ -3,15 +3,13 @@ from random import shuffle
 mylist = ["jack", "queen", "king"]
 for i in mylist:
     print(i)
-    print(mylist)
+
 
 myset = {"jack", "queen", "king"}
 
 for _ in myset:
     print(_)
 
-for _ in myset:
-    print(_)
 
 
     
